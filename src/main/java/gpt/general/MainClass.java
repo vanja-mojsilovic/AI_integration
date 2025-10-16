19ea2c428c848149d1b17d6d53cea77e2f908b2c
vanja-mojsilovic <vanja.mojsilovic@spothopperapp.com>
2025-10-16 11:17:36 +0200

    com theokanning openai service OpenAiService

--- src/main/java/gpt/general/MainClass.java
+++ src/main/java/gpt/general/MainClass.java
@@ -1,14 +1,14 @@
 package gpt.general;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import java.util.Scanner;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
-
+import java.util.Scanner;
 
 public class MainClass {
     private final String apiKey;
+
     public MainClass() {
         Dotenv dotenv = Dotenv.configure()
                 .ignoreIfMissing()
@@ -22,20 +22,19 @@ public class MainClass {
         }
     }
 
-
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            //System.out.print("Enter your question: ");
-            //String userQuestion = scanner.nextLine();
             String questionFilePath = "src/main/resources/question_file.txt";
             String userQuestion = new String(Files.readAllBytes(Paths.get(questionFilePath)));
-            System.out.print(userQuestion);
+
             String filePath = "src/main/resources/restaurant_info.txt";
+
             MainClass main = new MainClass();
             MethodsClass methodsClass = new MethodsClass(main.apiKey);
-            methodsClass.run(userQuestion,filePath);
+            methodsClass.run(userQuestion, filePath);
         } catch (Exception e) {
-            System.err.println(" Exception: " + e.getMessage());
+            System.err.println("Exception: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
